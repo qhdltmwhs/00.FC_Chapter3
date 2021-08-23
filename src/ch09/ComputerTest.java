d83a7bfb5f99273fcb5352139bfdd003cf8224bf
qhdltmwhs <qhdltmwhs@gmail.com>
2021-08-23 15:27:18 +0900

    .

---
+++ src/ch09/ComputerTest.java
@@ -0,0 +1,17 @@
+package ch09;
+
+public class ComputerTest {
+
+	public static void main(String[] args) {
+
+		Computer desktop = new Desktop();
+
+		/*
+		 * 추상 클래스 는 인스턴스화 할수없다. 
+		 * Cannot instantiate the type Computer
+		 */
+		// Computer computer = new Computer();
+		
+	}
+
+}
